d6599700a9a6bae190b3cdb32838070eeabfdf9b
ismael-15 <88355594+ismael-15@users.noreply.github.com>
2021-09-07 22:06:03 -0600

    Add files via upload

---
+++ practica07/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            dgData.Columns.Add("idStudent", "CODIGO");
+            dgData.Columns.Add("firstName", "NOMBRE");
+            dgData.Columns.Add("lasName", "APELLIDO");
+            dgData.Columns.Add("age", "EDAD");
+            dgData.Columns.Add("genero", "GENERO");
+
+            foreach(var student in selectAllData())
+            {
+                dgData.Rows.Add(student.idStudent, student.firstName, student.lastName, student.age, student.genero);
+
+
+            }
+        }
+        private List<Student> selectAllData()
+        {
+            List<Student> result = (
+                                    from student in Student.GetStudent()
+                                    select new Student()
+                                    {
+                                         idStudent = student.idStudent,
+                                         firstName = student.firstName,
+                                         lastName = student.lastName,
+                                         age = student.age,
+                                         genero = student.genero
+                                    }
+
+                ).ToList();
+
+            return result;
+
+
+        }
+    }
+}
---
+++ practica07/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica07
+{
+    class Student
+    {
+        public int idStudent { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public int age { get; set; }
+        public string genero { get; set; }
+
+        public static List<Student> GetStudent()
+        {
+            List<Student> student = new List<Student>
+            {
+                new Student { idStudent=1001, firstName="Darlyn", lastName="Luna",age=21, genero="femenino"},
+                new Student { idStudent=1002, firstName="Juan", lastName="Vijil",age=21, genero="masculino"},
+                new Student { idStudent=1003, firstName="Gabriel", lastName="Salmeron",age=19, genero="masculino"},
+                new Student { idStudent=1004, firstName="Antonio", lastName="Hernadez",age=18, genero="masculino"},
+                new Student { idStudent=1005, firstName="Sixmy", lastName="Claros",age=18,genero="masculino"},
+                new Student { idStudent=1006, firstName="Xenia", lastName="Argueta",age=17,genero="femenino"},
+                new Student { idStudent=1007, firstName="Ismael", lastName="Luna",age=19,genero="masculino"},
+            };
+
+            //valor de retorno, sera la lista de estuidantes
+            return student;
+        }
+    }
+}
